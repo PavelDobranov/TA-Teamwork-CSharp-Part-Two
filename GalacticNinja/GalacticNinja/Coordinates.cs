c8f3a0d6d89e71b963a210f2a749c280aa4eb403
PavelDobranov <pdobranov@gmail.com>
2015-02-16 15:31:28 +0200

    Added Coordinate.cs

---
+++ GalacticNinja/GalacticNinja/Coordinates.cs
@@ -0,0 +1,32 @@
+﻿namespace GalacticNinja
+{
+    using System;
+
+    public struct Coordinate
+    {
+        public Coordinate(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public int Row { get; set; }
+        public int Col { get; set; }
+
+        public static Coordinate operator +(Coordinate first, Coordinate second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Coordinate operator -(Coordinate first, Coordinate second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+    }
+}
