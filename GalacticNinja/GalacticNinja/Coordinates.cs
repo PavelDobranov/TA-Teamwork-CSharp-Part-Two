802c5290485c86ec130e5f2d5992b1a706f0e3a8
b-slavov <boyan.slavoff@gmail.com>
2015-02-16 16:20:18 +0200

    Revert "Delete Coordinates.cs"
    This reverts commit 066894448aa39f3f672c6e04efc5cd83ffe39695.

---
+++ GalacticNinja/GalacticNinja/Coordinates.cs
@@ -0,0 +1,32 @@
+﻿namespace GalacticNinja
+{
+    using System;
+
+    public struct Coordinate
+    {
+        public Coordinate(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+
+        public static Coordinate operator +(Coordinate first, Coordinate second)
+        {
+            return new Coordinate(first.Row + second.Row, first.Col + second.Col);
+        }
+
+        public static Coordinate operator -(Coordinate first, Coordinate second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(Coordinate other)
+        {
+            return this.Row == other.Row && this.Col == other.Col;
+        }
+    }
+}
