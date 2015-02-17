bddd1131b25113bd52d9e957657b5852c005c6c1
PavelDobranov <pdobranov@gmail.com>
2015-02-17 16:04:52 +0200

    Added Dragon.cs

---
+++ DwarfWarriorGame/DwarfWarrior/Dragon.cs
@@ -0,0 +1,21 @@
+﻿namespace DwarfWarrior
+{
+    using Interfaces;
+    using System;
+
+    public class Dragon : GameObject, IRenderable, ICollidable
+    {
+        private const int InitHealth = 1;
+        private const int InitDamage = 10;
+
+        public Dragon(Coordinate topLeftPosition, Coordinate speed, char[,] body)
+            : base(topLeftPosition, Dragon.InitHealth, Dragon.InitDamage, speed, body)
+        {
+        }
+
+        public override bool CanCollideWith(ICollidable other)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
