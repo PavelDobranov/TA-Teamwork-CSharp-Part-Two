e886ab29d37ae2e2aa84b36b12ca161da663c68f
PavelDobranov <pdobranov@gmail.com>
2015-02-17 18:26:49 +0200

    Added Player.cs

---
+++ DwarfWarriorGame/DwarfWarrior/Player.cs
@@ -0,0 +1,20 @@
+﻿namespace DwarfWarrior
+{
+    using Interfaces;
+
+    public class Player : GameObject, IRenderable, ICollidable
+    {
+        private const int InitHealth = 3;
+        private const int InitDamage = 10;
+
+        public Player(Coordinate topLeftPosition, Coordinate speed, char[,] body)
+            : base(topLeftPosition, Player.InitHealth, Player.InitDamage, speed, body)
+        {
+        }
+
+        public override bool CanCollideWith(ICollidable other)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
