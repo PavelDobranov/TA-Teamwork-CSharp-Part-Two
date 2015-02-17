a177bd83c333f272afa3008e32065500a4fad0b6
PavelDobranov <pdobranov@gmail.com>
2015-02-17 16:04:04 +0200

    Added Battlecruiser.cs

---
+++ DwarfWarriorGame/DwarfWarrior/Battlecruiser.cs
@@ -0,0 +1,20 @@
+﻿namespace DwarfWarrior
+{
+    using Interfaces;
+
+    public class Battlecruiser : GameObject, IRenderable, ICollidable
+    {
+        private const int InitHealth = 3;
+        private const int InitDamage = 10;
+
+        public Battlecruiser(Coordinate topLeftPosition, Coordinate speed, char[,] body)
+            : base(topLeftPosition, Battlecruiser.InitHealth, Battlecruiser.InitDamage, speed, body)
+        {
+        }
+
+        public override bool CanCollideWith(ICollidable other)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
