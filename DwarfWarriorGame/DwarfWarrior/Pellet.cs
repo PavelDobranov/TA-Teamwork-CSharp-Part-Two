99082c6748b85c3cbf99466259e11732d2fbbd7b
PavelDobranov <pdobranov@gmail.com>
2015-02-17 16:30:13 +0200

    Added Pallet.cs

---
+++ DwarfWarriorGame/DwarfWarrior/Pellet.cs
@@ -0,0 +1,20 @@
+﻿namespace DwarfWarrior
+{
+    using Interfaces;
+
+    public class Pellet : GameObject, IRenderable, ICollidable
+    {
+        private const int InitHealth = 1;
+        private const int InitDamage = 1;
+
+        public Pellet(Coordinate topLeftPosition, Coordinate speed, char[,] body)
+            : base(topLeftPosition, Pellet.InitHealth, Pellet.InitDamage, speed, body)
+        {
+        }
+
+        public override bool CanCollideWith(ICollidable other)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
