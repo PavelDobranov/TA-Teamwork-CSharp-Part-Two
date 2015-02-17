8022daa0db3d72062e3abcdd411ffbcc4c0324e8
PavelDobranov <pdobranov@gmail.com>
2015-02-17 16:04:31 +0200

    Added Carrier.cs

---
+++ DwarfWarriorGame/DwarfWarrior/Carrier.cs
@@ -0,0 +1,20 @@
+﻿namespace DwarfWarrior
+{
+    using Interfaces;
+
+    public class Carrier : GameObject, IRenderable, ICollidable
+    {
+        private const int InitHealth = 3;
+        private const int InitDamage = 10;
+
+        public Carrier(Coordinate topLeftPosition, Coordinate speed, char[,] body)
+            : base(topLeftPosition, Carrier.InitHealth, Carrier.InitDamage, speed, body)
+        {
+        }
+
+        public override bool CanCollideWith(ICollidable other)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
