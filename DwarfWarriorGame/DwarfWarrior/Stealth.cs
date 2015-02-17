d04766f1d7b01aed28730c55a938c448f3f4219f
PavelDobranov <pdobranov@gmail.com>
2015-02-17 16:05:17 +0200

    Added Stealth.cs

---
+++ DwarfWarriorGame/DwarfWarrior/Stealth.cs
@@ -0,0 +1,20 @@
+﻿namespace DwarfWarrior
+{
+    using Interfaces;
+
+    public class Stealth : GameObject, IRenderable, ICollidable
+    {
+        private const int InitHealth = 1;
+        private const int InitDamage = 10;
+
+        public Stealth(Coordinate topLeftPosition, Coordinate speed, char[,] body)
+            : base(topLeftPosition, Stealth.InitHealth, Stealth.InitDamage, speed, body)
+        {
+        }
+
+        public override bool CanCollideWith(ICollidable other)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
