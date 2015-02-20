0ddc5a02a3f72b70065967f8f13c1949459c8a8d
iwelina-popova <iwelina.popova@gmail.com>
2015-02-20 16:28:12 +0200

    Added Engine.cs

--- DwarfWarriorGame/DwarfWarrior/Program.cs
+++ DwarfWarriorGame/DwarfWarrior/Program.cs
@@ -6,15 +6,25 @@
     {
         static void Main()
         {
-            int rows = 25;
-            int cols = 25;
+            //int rows = 50;
+            //int cols = 50;
 
-            var renderer = new ConsoleRenderer(rows, cols);
+            //var renderer = new ConsoleRenderer(rows, cols);
+            //var factory = new GameObjectsFactory();
+            //var pesho = factory.ProduceObject(ObjectType.Dragon, 22, 15, 0, 0);
+
+            //renderer.AddToBuffer(pesho);
+            //renderer.RenderAll();
+            //Console.ReadLine();
+
+            var Engine = new Engine();
             var factory = new GameObjectsFactory();
-            var pesho = factory.ProduceObject(ObjectType.Dragon, 22, -1, 0, 0);
+            Engine.AddGameObject(factory.ProduceObject(ObjectType.Battlecruiser, 0, 50, 0, -1));
+            Engine.AddGameObject(factory.ProduceObject(ObjectType.Carrier, 5, 50, 0, -1));
+            Engine.AddGameObject(factory.ProduceObject(ObjectType.Dragon, 10, 60, 0, -1));
+            Engine.AddGameObject(factory.ProduceObject(ObjectType.Stealth, 20, 70, 0, -1));
 
-            renderer.AddToBuffer(pesho);
-            renderer.RenderAll();
+            Engine.Run();
             Console.ReadLine();
         }
     }
