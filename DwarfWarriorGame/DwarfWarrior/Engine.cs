ebb7ba2c8a6cb49795a8ceaee6cf4d38de69b90d
iwelina-popova <iwelina.popova@gmail.com>
2015-02-20 16:41:22 +0200

    Added Engine.cs

---
+++ DwarfWarriorGame/DwarfWarrior/Engine.cs
@@ -0,0 +1,48 @@
+﻿namespace DwarfWarrior
+{
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public class Engine
+    {
+        public const int gameRows = 25;
+        public const int gameCols = 50;
+        public const int gameSpeed = 100;
+        
+        private ConsoleRenderer renderer;
+        // game controller
+        List<GameObject> gameObjects;
+        // player
+
+
+        public Engine()
+        {
+            this.renderer = new ConsoleRenderer(Engine.gameRows, Engine.gameCols);
+            this.gameObjects = new List<GameObject>();
+        }
+
+        public void AddGameObject(GameObject obj)
+        {
+            this.gameObjects.Add(obj);
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                foreach (var gameObject in this.gameObjects)
+                {
+                    renderer.AddToBuffer(gameObject);
+                }
+
+                renderer.RenderAll();
+                Thread.Sleep(gameSpeed);
+                renderer.ClearBuffer();
+
+                foreach (var gameObject in this.gameObjects)
+                {
+                    gameObject.Update();
+                }
+            }
+        }
+    }
+}
