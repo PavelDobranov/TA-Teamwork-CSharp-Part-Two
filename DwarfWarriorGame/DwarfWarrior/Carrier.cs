231790395b3ae57c3fd351ec5865fab9dfdc0e9c
iwelina-popova <iwelina.popova@gmail.com>
2015-02-20 18:41:32 +0200

    Implemented CanCollideWith

--- DwarfWarriorGame/DwarfWarrior/Carrier.cs
+++ DwarfWarriorGame/DwarfWarrior/Carrier.cs
@@ -14,7 +14,9 @@
 
         public override bool CanCollideWith(ICollidable other)
         {
-            throw new System.NotImplementedException();
+            return other.Type == ObjectType.Pellet ||
+                   other.Type == ObjectType.Player ||
+                   other.Type == ObjectType.Shell;
         }
     }
 }
