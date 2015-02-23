c40d45100c7b2ff97df580aec8686dbb74b15a77
Ivan Aleksandrov <vancuver@gmail.com>
2015-02-23 21:32:32 +0200

    HighScore
    Added highscore + nickname input upon start

--- DwarfWarriorGame/DwarfWarrior/Program.cs
+++ DwarfWarriorGame/DwarfWarrior/Program.cs
@@ -1,11 +1,70 @@
 ﻿namespace DwarfWarrior
 {
     using System;
+    using System.Collections;
+    using System.IO;
 
     class Program
     {
+        private static int currentScore = 0;
+        private static string nickName;
+
+        public static void SaveScore()
+        {
+            ArrayList scores = new ArrayList(10);
+            string line = "";
+            int lineCount = 0;
+
+            try
+            {
+                StreamReader reader = new StreamReader("..\\..\\High.Score");
+                //reads all scores a save them in a List
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int indexOfSplit = line.IndexOf(' ');
+                    string points = line.Substring(0, indexOfSplit);
+                    string name = line.Substring(indexOfSplit + 1);
+                    string[] score = new string[] { points, name };
+                    scores.Add(score);
+                    lineCount++;
+                }
+                reader.Close();
+                //Append new high score if they are less than 10
+                int finalPos = 0;
+                foreach (string[] scoreLines in scores)
+                {
+                    if (currentScore > int.Parse(scoreLines[0])) break;
+                    finalPos++;
+                    if (finalPos > 9) break;
+                }
+                scores.Insert(finalPos, new string[] { currentScore.ToString(), nickName });
+                StreamWriter writer = new StreamWriter("..\\..\\High.Score");
+                for (int i = 0; i < 10 && i < scores.Count; i++)
+                {
+                    writer.WriteLine(((string[])scores[i])[0] + " " + ((string[])scores[i])[1]);
+                }
+                writer.Close();
+            }
+            catch (SystemException)
+            {
+                Console.WriteLine("Could not write to high score file, delete it to resolve conflict.");
+            }
+        }
+
         static void Main()
         {
+            //set nickname on start
+            nickName = string.Empty;
+            while (nickName == string.Empty)
+            {
+                Console.CursorVisible = true;
+                Console.SetCursorPosition(35, 16);
+                Console.Write("Enter Nickname: ");
+                nickName = Console.ReadLine().Trim();
+                Console.CursorVisible = false;
+            }
+            Console.Clear();
             //int rows = 50;
             //int cols = 50;
 
